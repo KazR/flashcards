16ecf4bcf2dd7853b6e1e37d2a03ac737cabb150
Kaz <qasemrana117@gmail.com>
2023-05-03 00:09:05 +0100

    Now allows for the front and back of the card to be swapped

--- app.py
+++ app.py
@@ -10,32 +10,43 @@ flashcards = pd.read_excel(file_path)
 
 # Create a Tkinter window
 window = tk.Tk()
-window.geometry("250x175")  # Set the window size to 400x400
+window.geometry("250x250")  # Set the window size to 400x400
 
 # Declare the back variable as global
 back = ""
 previous_card = None
+is_swapped = False
+
 
 # Function to check the user's answer and proceed to the next flashcard
 def check_answer():
-    global back
+    global back, is_swapped
+    
     user_answer = entry.get().lower()
-    if user_answer == back.lower():
-        result_label.config(text="Correct!", fg="green")
+    if is_swapped:
+        correct_side = front
     else:
-        result_label.config(text="Wrong! The correct answer is: " + back, fg="red")
-
+        correct_side = back
+    if user_answer == correct_side.lower():
+        result_label.config(text="Correct!", fg="green")
+        entry.focus_set() 
+    else:
+        result_label.config(text="Wrong! The correct answer is: " + correct_side, fg="red")
+        entry.focus_set() 
     check_button.config(text="Next")
+    check_button.focus_set()
     check_button.unbind('<Return>')
     check_button.bind('<Return>', handle_next)
-    check_button.focus_set()
+
 
 def handle_next(event):
     display_flashcard()
 
 def display_flashcard():
-    global back, previous_card
+    global front, back, previous_card
+
     # Randomly select a row
+
     rows = flashcards.index.tolist()
     if previous_card is not None:
         rows.remove(previous_card)
@@ -48,14 +59,17 @@ def display_flashcard():
 
     # Update the flashcard window
     window.title("Flashcard Application")
-    front_label.config(text="Front of the card: " + front)
+    if is_swapped:
+        front_label.config(text="Front of the card: " + back)
+    else:
+        front_label.config(text="Front of the card: " + front)
     entry.delete(0, tk.END)
     result_label.config(text="")
     check_button.config(text="Check Answer")
     check_button.unbind('<Return>')
     check_button.bind('<Return>', handle_check)
     check_button.focus_set()
-    entry.focus_set()  # Set focus to the entry widget
+
 
 def handle_check(event):
     check_answer()
@@ -76,9 +90,24 @@ check_button = tk.Button(window, text="Check Answer", command=check_answer)
 check_button.pack(pady=10)
 check_button.bind('<Return>', handle_check)
 
+def swap_flashcard():
+    global front, back, is_swapped
+    is_swapped = not is_swapped
+    if is_swapped:
+        front_label.config(text="Front of the card: " + back)
+    else:
+        front_label.config(text="Front of the card: " + front)
+    
+def handle_next(event):
+    display_flashcard()
+
 # Label to display the result
 result_label = tk.Label(window, text="")
 result_label.pack(pady=10)
+
+# Button to swap the front and back of the flashcard
+swap_button = tk.Button(window, text="Swap", command=swap_flashcard)
+swap_button.pack(pady=10)
 
 # Start the flashcard application
 display_flashcard()
